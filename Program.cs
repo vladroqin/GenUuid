1165d38feb03efd2fa4dcefc4f41cdabe4703627
Vlad Roqin <roqin@yandex.ru>
2023-02-05 00:47:53 +0300

    Change file to stream

--- Program.cs
+++ Program.cs
@@ -11,11 +11,6 @@ namespace GenUuid;
 
 public static class ExtractUuid
 {
-#if DEBUG
-  private static string _log_fn =
-      $"{Environment.GetFolderPath(Environment.SpecialFolder.Personal)}/genuuid.log";
-#endif
-
   /// Извлечение UUID из подходящей строки
   /// </summary>
   /// <param name="s">Строка</param>
@@ -38,7 +33,7 @@ public static class ExtractUuid
     {
       hash[3], hash[2], hash[1], hash[0], hash[5], hash[4], hash[7], hash[6],
       hash[8], hash[9], hash[10], hash[11], hash[12], hash[13], hash[14], hash[15]
-};
+    };
     return sum;
   }
 
@@ -50,15 +45,23 @@ public static class ExtractUuid
   /// <param name="f">Способ работы</param>
   /// <typeparam name="T">Тип результата</typeparam>
   /// <returns>Результат</returns>
-  private static T XmlInZip<T>(string file, string fileInArchive, Func<StreamReader, T> f)
+  private static T XmlInZip<T>(Stream stream, string fileInArchive, Func<StreamReader, T> f)
   {
-    using var stream = File.OpenRead(file);
-    using var arc = new ZipArchive(stream, ZipArchiveMode.Read);
-    using var sr = new StreamReader(arc.GetEntry(fileInArchive).Open());
+    ZipArchive? arc = default;
+    T? result = default;
+    try
+    {
+      arc = new ZipArchive(stream, ZipArchiveMode.Read);
+      using var sr = new StreamReader(arc.GetEntry(fileInArchive).Open());
+      result = f.Invoke(sr);
+    }
+    catch (Exception e)
+    {
 #if DEBUG
-    Log.Information(file);
+      Log.Warning(e.Message);
 #endif
-    return f.Invoke(sr);
+    }
+    return (T)result;
   }
 
   /// <summary>
@@ -66,7 +69,7 @@ public static class ExtractUuid
   /// </summary>
   /// <param name="stream">Поток</param>
   /// <returns>Результат</returns>
-  private static Guid Docx15(StreamReader stream)
+  private static Guid? Docx15(StreamReader stream)
   {
     var xdoc = XDocument.Load(stream);
     XNamespace w = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
@@ -79,7 +82,10 @@ public static class ExtractUuid
     if (!string.IsNullOrWhiteSpace(eid))
       Log.Information($"DOCX docId.w15 <<{eid}>>");
 #endif
-    return new Guid(eid);
+    if(Guid.TryParse(eid, out Guid result))
+      return result;
+    else
+      return null;
   }
 
   /// <summary>
@@ -87,7 +93,7 @@ public static class ExtractUuid
   /// </summary>
   /// <param name="stream">Поток</param>
   /// <returns>Результат</returns>
-  private static Guid Docx(StreamReader stream)
+  private static Guid? DocxNS(StreamReader stream)
   {
     var xdoc = XDocument.Load(stream);
     XNamespace od = "http://schemas.openxmlformats.org/officeDocument/2006/custom-properties";
@@ -100,11 +106,14 @@ public static class ExtractUuid
     if (!string.IsNullOrWhiteSpace(eid))
       Log.Information($"DOCX Property fmtid <<{eid}>>");
 #endif
-    return new Guid(eid);
+    if(Guid.TryParse(eid, out Guid result))
+      return result;
+    else
+      return null;
   }
 
   /// <summary>
-  /// Находим путь к файлу с информацией. Если его нет бросаем исключение
+  /// Находим путь к файлу с информацией в EPUB. Если его нет бросаем исключение
   /// </summary>
   /// <param name="stream">Поток</param>
   /// <returns>Путь к файлу .opf</returns>
@@ -129,11 +138,11 @@ public static class ExtractUuid
   }
 
   /// <summary>
-  /// Находим UUID
+  /// Находим UUID в EPUB
   /// </summary>
   /// <param name="stream">Поток</param>
   /// <returns>UUID</returns>
-  private static Guid Content(StreamReader stream)
+  private static Guid? Content(StreamReader stream)
   {
     var xdoc = XDocument.Load(stream);
     XNamespace opf = "http://www.idpf.org/2007/opf";
@@ -232,13 +241,43 @@ public static class ExtractUuid
       }
     }
 
+    return result;
+  }
+
+  /// <summary>
+  /// Что-то делаем с файлом
+  /// </summary>
+  /// <param name="file">Имя файла</param>
+  /// <param name="f">Действие</param>
+  /// <returns>Результат</returns>
+  private static Guid DefAct(string file, Func<Stream, Guid>? f = null)
+  {
+    f = f ?? Default;
+    Stream? stream = default;
+    Guid? result = default;
+#if DEBUG
+    Log.Information($"Work with {file}");
+#endif
+    try
+    {
+      stream = File.OpenRead(file);
+      result = f.Invoke(stream);
+    }
+    catch (Exception e)
+    {
+#if DEBUG
+      Log.Information(e.Message);
+#endif
+      stream?.Close();
+    }
     return (Guid)result;
   }
 
   static ExtractUuid()
   {
 #if DEBUG
-    Log.Logger = new LoggerConfiguration().WriteTo.File(_log_fn).CreateLogger();
+    string log_fn = $"{Environment.GetFolderPath(Environment.SpecialFolder.Personal)}/genuuid.log";
+    Log.Logger = new LoggerConfiguration().WriteTo.File(log_fn).CreateLogger();
 #endif
     Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
   }
@@ -262,9 +301,11 @@ public static class ExtractUuid
           if (File.Exists(file))
             Console.WriteLine($"{GetUuidByFN(file)}  {file}");
         }
-        catch (UnauthorizedAccessException e)
+        catch (Exception e)
         {
-          Console.Error.WriteLine(e.Message);
+#if DEBUG
+          Log.Warning(e.Message);
+#endif
         }
     }
   }
@@ -281,26 +322,26 @@ public static class ExtractUuid
     switch (ext)
     {
       case ".pdf":
-        uuid = Pdf(file);
+        uuid = DefAct(file, Pdf);
         break;
       case ".epub":
-        uuid = Epub(file);
+        uuid = DefAct(file, Epub);
         break;
       case ".fbd":
       case ".fb2":
-        uuid = Fb2(file);
+        uuid = DefAct(file, Fb2);
         break;
       //case ".docm":
       /* Еще можно добавить попытку извлечения для других форматов МС Офиса,
        * но они либо его в готовом виде не содержат, либо встречаются крайне редко
        * (у меня).
        * Также можно добавить в будущем поддержку XPS, но по нему я дополнительно
-       * информации найти не смог. */
+       * информации найти не смог :(. */
       case ".docx":
-        uuid = Docx(file);
+        uuid = DefAct(file, Docx);
         break;
       default:
-        uuid = Default(file);
+        uuid = DefAct(file, Default);
         break;
     }
     return uuid;
@@ -309,13 +350,12 @@ public static class ExtractUuid
   /// <summary>
   /// Извлечение UUID из ПДФ
   /// </summary>
-  /// <param name="file">Имя файла</param>
+  /// <param name="stream">Поток</param>
   /// <returns>Результат</returns>
   /// <summary>
-  public static Guid Pdf(string file)
+  public static Guid Pdf(Stream stream)
   {
     Guid? result = null;
-    using var stream = File.OpenRead(file);
     using (var pdf = new PdfReader(stream))
     {
       /* Исхожу из того, что в файле есть метаинформация в XMP. Насколько
@@ -343,14 +383,14 @@ public static class ExtractUuid
                 ?.Value;
 #if DEBUG
             if (!string.IsNullOrWhiteSpace(preresult))
-              Log.Information($"PDF {file} DocumentID <<{preresult}>>");
+              Log.Information($"PDF DocumentID <<{preresult}>>");
 #endif
             result = preresult?.GetUuidFromString();
           }
           catch (Exception e)
           {
 #if DEBUG
-            Log.Warning($"{file}: {e.Message}");
+            Log.Warning(e.Message);
 #endif
           }
         }
@@ -362,26 +402,25 @@ public static class ExtractUuid
         var preresult = ChangeByteOrder(pdfarr[0].GetBytes());
 #if DEBUG
         if (preresult != null && preresult.Count() != 0)
-          Log.Information($"PDF {file} Trailer <<{BitConverter.ToString(preresult)}>>");
+          Log.Information($"PDF Trailer <<{BitConverter.ToString(preresult)}>>");
 #endif
         result = new Guid(ChangeByteOrder(pdfarr[0].GetBytes()));
       }
     }
     //Если уж и здесь нет, считаем MD5
     if (result == null)
-      result = Default(file);
+      result = Default(stream);
     return (Guid)result;
   }
 
   /// <summary>
   /// Извлечение UUID из FB2
   /// </summary>
-  /// <param name="file">Имя файла</param>
+  /// <param name="stream">Поток</param>
   /// <returns>Результат</returns>
-  public static Guid Fb2(string file)
+  public static Guid Fb2(Stream stream)
   {
     Guid? result = null;
-    using var stream = File.OpenRead(file);
     //Пробуем взять из /FictionBook/description/document-info/id
     try
     {
@@ -396,7 +435,7 @@ public static class ExtractUuid
       if (!string.IsNullOrWhiteSpace(fb2Id))
       {
 #if DEBUG
-        Log.Information($"FB2 {file} id <<{fb2Id}>>");
+        Log.Information($"FB2 id <<{fb2Id}>>");
 #endif
         result = Guid.Parse(fb2Id);
       }
@@ -404,9 +443,9 @@ public static class ExtractUuid
     catch (Exception e)
     {
 #if DEBUG
-      Log.Warning($"{file}: {e.Message}");
+      Log.Warning(e.Message);
 #endif
-      result = Default(file);
+      result = Default(stream);
     }
     return (Guid)result;
   }
@@ -414,88 +453,123 @@ public static class ExtractUuid
   /// <summary>
   /// Извлечение UUID из Epub
   /// </summary>
-  /// <param name="file">Имя файла</param>
-  /// <returns>UUID</returns>
-  public static Guid Epub(string file)
+  /// <param name="stream">Поток</param>
+  /// <returns>Результат</returns>
+  public static Guid Epub(Stream stream)
   {
     string? content_opf = null;
     try
     {
-      content_opf = XmlInZip(file, "META-INF/container.xml", Container);
+      content_opf = XmlInZip(stream, "META-INF/container.xml", Container);
     }
     catch (Exception e)
-        when (e is not FileNotFoundException && e is not UnauthorizedAccessException)
     {
 #if DEBUG
-      Log.Warning($"{file}: {e.Message}");
+      Log.Warning(e.Message);
 #endif
     }
     if (content_opf != null)
     {
       try
       {
-        var eid = XmlInZip(file, content_opf, Content);
-        return eid;
+        var eid = XmlInZip(stream, content_opf, Content);
+        if (eid != null)
+         return (Guid)eid;
+        else throw new Exception();
       }
       catch (Exception e)
       {
 #if DEBUG
-        Log.Warning($"{file}: {e.Message}");
+        Log.Warning(e.Message);
 #endif
       }
     }
-    return Default(file);
+    return Default(stream);
   }
+
+  /// <summary>
+  /// Извлечение UUID из Docx
+  /// </summary>
+  /// <param name="stream">Поток</param>
+  /// <returns>Результат</returns>
+  public static Guid Docx(Stream stream)
+  {
+    Guid? result = default;
+    try
+    {
+      result = XmlInZip(stream, "word/settings.xml", Docx15);
+    }
+    catch (Exception e)
+    {
+#if DEBUG
+      Log.Warning(e.Message);
+#endif
+    }
+    if (result == default)
+    {
+      try
+      {
+        result = XmlInZip(stream, "docProps/custom.xml", DocxNS);
+      }
+      catch (Exception e)
+      {
+#if DEBUG
+        Log.Warning(e.Message);
+#endif
+      }
+    }
+    if(result == default)
+      result = Default(stream);
+    return (Guid)result;
+  }
+
+  /// <summary>
+  /// Получение UUID из файла любого типа
+  /// </summary>
+  /// <param name="stream">Поток</param>
+  /// <returns>Результат</returns>
+  public static Guid Default(Stream stream)
+  {
+    using var md5 = MD5.Create();
+    var sum = md5.ComputeHash(stream);
+#if DEBUG
+    Log.Information($"Default md5sum");
+#endif
+    return new Guid(ChangeByteOrder(sum));
+  }
+
+  /// <summary>
+  /// Извлечение UUID из ПДФ
+  /// </summary>
+  /// <param name="file">Имя файла</param>
+  /// <returns>Результат</returns>
+  public static Guid Pdf(string file) => DefAct(file, Pdf);
+
+  /// <summary>
+  /// Извлечение UUID из FB2
+  /// </summary>
+  /// <param name="file">Имя файла</param>
+  /// <returns>Результат</returns>
+  public static Guid Fb2(string file) => DefAct(file, Fb2);
+
+  /// <summary>
+  /// Извлечение UUID из Epub
+  /// </summary>
+  /// <param name="file">Имя файла</param>
+  /// <returns>Результат</returns>
+  public static Guid Epub(string file) => DefAct(file, Epub);
 
   /// <summary>
   /// Извлечение UUID из Docx
   /// </summary>
   /// <param name="file">Имя файла</param>
   /// <returns>Результат</returns>
-  public static Guid Docx(string file)
-  {
-    Guid? result = null;
-    try
-    {
-      result = XmlInZip(file, "word/settings.xml", Docx15);
-    }
-    catch (Exception e)
-        when (e is not FileNotFoundException && e is not UnauthorizedAccessException)
-    {
-#if DEBUG
-      Log.Warning($"{file}: {e.Message}");
-#endif
-    }
-    if (result == null)
-    {
-      try
-      {
-        result = XmlInZip(file, "docProps/custom.xml", Docx);
-      }
-      catch (Exception e)
-      {
-#if DEBUG
-        Log.Warning($"{file}: {e.Message}");
-#endif
-        result = Default(file);
-      }
-    }
-    return (Guid)result;
-  }
+  public static Guid Docx(string file) => DefAct(file, Docx);
 
   /// <summary>
-  /// Получение UUID из любого файла
+  /// Получение UUID из файла любого типа
   /// </summary>
   /// <param name="file">Имя файла</param>
   /// <returns>Результат</returns>
-  public static Guid Default(string file)
-  {
-    using var stream = File.OpenRead(file);
-    using var md5 = MD5.Create();
-    var sum = md5.ComputeHash(stream);
-#if DEBUG
-    Log.Information($"Default {file} md5sum");
-#endif
-    return new Guid(ChangeByteOrder(sum));
-  }
+  public static Guid Default(string file) => DefAct(file, Default);
 }
